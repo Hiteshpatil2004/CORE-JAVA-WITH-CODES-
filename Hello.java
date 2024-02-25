6b37eb305ac1ebdb72f3fed6ea076ecac08820e5
Hiteshpatil2004 <155521032+Hiteshpatil2004@users.noreply.github.com>
2024-02-25 13:05:52 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+//create class using file name note that file name and class name will be same and the first letter can be in capital form
+class Hello
+{
+  public static void main(String args[])
+{
+  system.out.println("HELLO WORLD");
+}
+}
+/*
+
+  RUN COMMAND:-
+  1.javac Filename.java
+  2.java Filename
+  
+*/
